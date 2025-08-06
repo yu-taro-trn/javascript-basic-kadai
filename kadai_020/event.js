a61c22309cae2db66220648bf772cbc969f1dd54
yu-taro-trn <yu.arche.gmfn@gmail.com>
2025-08-06 15:21:55 +0900

    アップロード

---
+++ kadai_020/event.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById("btn");
+
+const text = document.getElementById('text');
+
+btn.addEventListener("click", () => {
+
+  const childList = document.createElement('li');
+
+  text.appendChild(childList);
+
+  text.textContent    = "ボタンをクリックしました。";
+  
+});
